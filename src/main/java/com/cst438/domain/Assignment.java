5c6f1fa881c29402e7d917bfdcce1dab9a95014f
David Gil <148482687+dlindnercsumb@users.noreply.github.com>
2025-03-12 14:33:25 +0000

    Added code for entities

--- src/main/java/com/cst438/domain/Assignment.java
+++ src/main/java/com/cst438/domain/Assignment.java
@@ -3,7 +3,6 @@ package com.cst438.domain;
 import jakarta.persistence.*;
 
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 
 @Entity
 public class Assignment {
@@ -12,8 +11,44 @@ public class Assignment {
     @Column(name="assignment_id")
     private int assignmentId;
  
-    // TODO  complete this class
-    // add additional attributes for title, dueDate
-    // add relationship between assignment and section entities
-    // add getter and setter methods
+    private String title;
+
+    @Column(name = "due_date")
+    private Date dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "section_no")
+    private Section section;
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -9,9 +9,45 @@ public class Enrollment {
     @Column(name="enrollment_id")
     int enrollmentId;
 	
-	// TODO complete this class
-    // add additional attribute for grade
-    // create relationship between enrollment and user entities
-    // create relationship between enrollment and section entities
-    // add getter/setter methods
+	private String grade;
+
+    @ManyToOne
+    @JoinColumn(name = "section_no")
+    private Section section;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public void setEnrollmentId(int enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
