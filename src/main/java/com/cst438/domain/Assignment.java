fa8c2185f64f38d426b0349b45b7ad78cb52ffb8
Omar Spiller <ospillerhernandez@csumb.edu>
2025-03-16 01:58:54 -0700

    Complete entity classes for enrollment, assignment, and grade. Code grade controller.

--- src/main/java/com/cst438/domain/Assignment.java
+++ src/main/java/com/cst438/domain/Assignment.java
@@ -1,19 +1,61 @@
 package com.cst438.domain;
 
-import jakarta.persistence.*;
-
 import java.sql.Date;
-import java.text.SimpleDateFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Assignment {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="assignment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id")
     private int assignmentId;
- 
-    // TODO  complete this class
-    // add additional attributes for title, dueDate
-    // add relationship between assignment and section entities
-    // add getter and setter methods
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "due_date")
+    private Date dueDate;
+
+    @ManyToOne
+    @JoinColumn(name="section_no")
+    private Section section;
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
--- src/main/java/com/cst438/domain/Grade.java
+++ src/main/java/com/cst438/domain/Grade.java
@@ -1,17 +1,60 @@
 package com.cst438.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Grade {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="grade_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "grade_id")
     private int gradeId;
- 
-    // TODO complete this class
-    // add additional attribute for score that is nullable
-    // add relationship between grade and assignment entities
-    // add relationship between grade and enrollment entities
-    // add getter/setter methods
+
+    @Column(name = "score", nullable = true)
+    private Integer score;
+
+    @ManyToOne
+    @JoinColumn(name = "assignment_id")
+    private Assignment assignment;
+
+    @ManyToOne
+    @JoinColumn(name = "enrollment_id")
+    private Enrollment enrollment;
+
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    public Enrollment getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Enrollment enrollment) {
+        this.enrollment = enrollment;
+    }
 }
