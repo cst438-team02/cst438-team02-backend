dbb73100d84e392fa97e1956c26d232873f3e48f
Scott Lam <slam@csumb.edu>
2025-03-18 03:45:09 -0700

    Coded EnrollmentController and StudentScheduleController
    Coded EnrollmentController and StudentScheduleController.

--- src/main/java/com/cst438/controller/EnrollmentController.java
+++ src/main/java/com/cst438/controller/EnrollmentController.java
@@ -1,6 +1,6 @@
 package com.cst438.controller;
 
-
+import com.cst438.domain.*;
 import com.cst438.dto.EnrollmentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +14,8 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class EnrollmentController {
 
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
 
     /**
      instructor gets list of enrollments for a section
@@ -22,13 +24,31 @@ public class EnrollmentController {
      */
     @GetMapping("/sections/{sectionNo}/enrollments")
     public List<EnrollmentDTO> getEnrollments(
-            @PathVariable("sectionNo") int sectionNo ) {
-
-        // TODO
-		//  hint: use enrollment repository findEnrollmentsBySectionNoOrderByStudentName method
-        //  remove the following line when done
-
-        return null;
+        @PathVariable("sectionNo") int sectionNo ) {
+        // using enrollment Repository find enrollment entities by Section number ordered by student name
+        List<Enrollment> enrollments = enrollmentRepository.findBySectionSectionNoOrderByStudentName(sectionNo);
+        List<EnrollmentDTO> dto_list = new ArrayList<>(); // to hold enrollment DTOs
+        // for each enrollment found, create an enrollment DTO then add it to list of DTOs
+        for (Enrollment e : enrollments) {
+            dto_list.add(new EnrollmentDTO(
+                    e.getEnrollmentId(),
+                    e.getGrade(),
+                    e.getStudent().getId(),
+                    e.getStudent().getName(),
+                    e.getStudent().getEmail(),
+                    e.getSection().getCourse().getCourseId(),
+                    e.getSection().getCourse().getTitle(),
+                    e.getSection().getSecId(),
+                    e.getSection().getSectionNo(),
+                    e.getSection().getBuilding(),
+                    e.getSection().getRoom(),
+                    e.getSection().getTimes(),
+                    e.getSection().getCourse().getCredits(),
+                    e.getSection().getTerm().getYear(),
+                    e.getSection().getTerm().getSemester()
+            ));
+        }
+        return dto_list;
     }
 
     // instructor uploads enrollments with the final grades for the section
@@ -40,13 +60,19 @@ public class EnrollmentController {
      */
     @PutMapping("/enrollments")
     public void updateEnrollmentGrade(@RequestBody List<EnrollmentDTO> dlist) {
-
-        // TODO
-
-        // For each EnrollmentDTO in the list
-        //  find the Enrollment entity using enrollmentId
-        //  update the grade and save back to database
-
+        // for each EnrollmentDTO in the list
+        for (EnrollmentDTO eDTO : dlist) {
+            //  find the Enrollment entity using enrollmentId
+            Enrollment e = enrollmentRepository.findById(eDTO.enrollmentId()).orElse(null);
+            // if Enrollment entity not found, return not found error
+            if (e==null) {
+                throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "enrollment not found "+eDTO.enrollmentId());
+            }
+            //  update the grade and save back to database
+            else {
+                e.setGrade(eDTO.grade());
+                enrollmentRepository.save(e);
+            }
+        }
     }
-
 }
